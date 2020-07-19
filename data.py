45b721af3cb55d51cf73c95df36f20e568951a95
Tai Lucas <tglucas@gmail.com>
2020-07-19 15:31:04 +0200

    Fix missing import.

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 import simplejson as json
 import umsgpack
 
-from ..datetime import make_timestamp
+from .datetime import make_timestamp
 
 def make_payload(timestamp=None, data=None):
     payload = {'timestamp': make_timestamp(timestamp, True)}
