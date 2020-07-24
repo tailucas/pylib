924df1a02e929154af753ae8afa95e0db1657e05
Tai Lucas <tglucas@gmail.com>
2020-07-24 22:25:37 +0200

    Add application bootstrap fixtures.

---
+++ __init__.py
@@ -0,0 +1,68 @@
+import boto3
+import builtins
+import logging
+import logging.handlers
+import os
+import os.path
+import sentry_sdk
+import sys
+import zmq
+
+from configparser import ConfigParser
+
+
+builtins.APP_NAME = os.path.basename(__file__)
+if sys.stdout.isatty() and os.system('systemctl status app') == 0:
+    print("{} is already running. Use 'systemctl stop app' to stop first.".format(APP_NAME))
+    sys.exit(1)
+
+# set the working directory for libraries that assume this (such as PyDrive)
+DIR = os.path.abspath(os.path.dirname(__file__))
+os.chdir(DIR)
+
+config = ConfigParser()
+config.optionxform = str
+config.read([os.path.join(DIR, '{}.conf'.format(APP_NAME))])
+builtins.DEVICE_NAME = config.get('app', 'device_name')
+
+sentry_sdk.init(
+    dsn=config.get('sentry', 'dsn'),
+    integrations=[FlaskIntegration()]
+)
+builtins.zmq_context = zmq.Context()
+zmq_context.setsockopt(zmq.LINGER, 0)
+
+# shutdown flag
+builtins.shutting_down = False
+# threads to interrupt
+builtins.interruptable_sleep = threading.Event()
+# threads to nanny
+builtins.threads_tracked = set()
+
+builtins.log = logging.getLogger(APP_NAME)
+# do not propagate to console logging
+log.propagate = False
+# DEBUG logging until startup complete
+log.setLevel(logging.DEBUG)
+syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
+formatter = logging.Formatter('%(name)s %(threadName)s [%(levelname)s] %(message)s')
+syslog_handler.setFormatter(formatter)
+log.addHandler(syslog_handler)
+if sys.stdout.isatty():
+    log.warning("Using console logging because there is a tty.")
+    stream_handler = logging.StreamHandler(stream=sys.stdout)
+    stream_handler.setFormatter(formatter)
+    log.addHandler(stream_handler)
+# set up application metrics
+builtins.boto3_session = boto3.Session()
+builtins.app_metrics = boto3_session.client('cloudwatch')
+
+from pylib.datetime import is_list, make_timestamp, parse_datetime
+from pylib.data import make_payload
+from pylib.aws.metrics import post_count_metric
+from pylib.aws.swf import SWFActivityWaiter, \
+    swf_exception_handler, \
+    DeviceInfoActivity, \
+    DeviceWorkflow
+from pylib.process import SignalHandler
+from pylib.threads import thread_nanny
