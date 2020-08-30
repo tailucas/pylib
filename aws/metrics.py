5d582dc117347b6f065faba261c021b42cab9221
Tai Lucas <tglucas@gmail.com>
2020-08-30 23:01:21 +0200

    Support additional metric dimensions.

--- aws/metrics.py
+++ aws/metrics.py
@@ -9,8 +9,24 @@ log = logging.getLogger(APP_NAME)
 app_metrics = boto3_session.client('cloudwatch')
 
 
-def post_count_metric(metric_name, count=1, unit='Count'):
+def post_count_metric(metric_name, count=1, unit='Count', dimensions=None):
     global app_metrics
+    metric_dimensions = [
+        {
+            'Name': 'Application',
+            'Value': APP_NAME
+        },
+        {
+            'Name': 'Device',
+            'Value': DEVICE_NAME
+        },
+    ]
+    if isinstance(dimensions, dict):
+        for k, v in list(dimensions.items()):
+            metric_dimensions.append({
+                'Name': k,
+                'Value': v
+            })
     try:
         app_metrics.put_metric_data(
             Namespace='automation',
@@ -18,16 +34,7 @@ def post_count_metric(metric_name, count=1, unit='Count'):
                 {
                     'MetricName': metric_name,
                     'Value': count,
-                    'Dimensions': [
-                        {
-                            'Name': 'Application',
-                            'Value': APP_NAME
-                        },
-                        {
-                            'Name': 'Device',
-                            'Value': DEVICE_NAME
-                        },
-                    ],
+                    'Dimensions': metric_dimensions,
                     'Timestamp': make_timestamp(),
                     'Unit': unit
                 },
