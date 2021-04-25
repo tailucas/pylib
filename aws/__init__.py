6a87d0ac41ac6b8d5bd95b04bd30b86c375de221
Tai Lucas <tglucas@gmail.com>
2021-04-25 10:05:57 +0200

    Fix boto bootstrap.

--- aws/__init__.py
+++ aws/__init__.py
@@ -3,6 +3,9 @@ from botocore.session import Session as BotoCoreSession
 
 boto_session = BotoCoreSession()
 boto3_session = Session(
-    aws_access_key_id=creds.aws_akid, # pylint: disable=undefined-variable
-    aws_secret_access_key=creds.aws_sak, # pylint: disable=undefined-variable
+    aws_access_key_id=creds_config.aws_akid, # pylint: disable=undefined-variable
+    aws_secret_access_key=creds_config.aws_sak, # pylint: disable=undefined-variable
     botocore_session=boto_session)
+
+swf_region = APP_CONFIG.get('botoflow', 'region') # pylint: disable=undefined-variable
+swf_domain = APP_CONFIG.get('botoflow', 'domain') # pylint: disable=undefined-variable
