76129f7f1a39352f68b78fc8c1ce6b9cb2079659
Tai Lucas <tglucas@gmail.com>
2021-04-25 14:00:54 +0200

    Patch in AWS environment variables from credential provider.

--- aws/__init__.py
+++ aws/__init__.py
@@ -1,5 +1,18 @@
+import logging
+import os
+
 from boto3 import Session
 from botocore.session import Session as BotoCoreSession
+
+log = logging.getLogger(APP_NAME) # pylint: disable=undefined-variable
+
+try:
+    os.environ['AWS_ACCESS_KEY_ID']
+    os.environ['AWS_SECRET_ACCESS_KEY']
+except KeyError:
+    log.debug('AWS environmnent variables unset. Setting in Python from credential provider.')
+    os.environ['AWS_ACCESS_KEY_ID'] = creds_config.aws_akid # pylint: disable=undefined-variable
+    os.environ['AWS_SECRET_ACCESS_KEY'] = creds_config.aws_sak # pylint: disable=undefined-variable
 
 boto_session = BotoCoreSession()
 boto3_session = Session(
