455fcc2a34d76241ce4cc1e1a8d8ef4388e98368
Tai Lucas <tglucas@gmail.com>
2021-04-25 09:30:13 +0200

    Simplify botocore and boto3 Session setup.

--- aws/__init__.py
+++ aws/__init__.py
@@ -1,9 +1,8 @@
-import boto3
-import botocore
+from boto3 import Session
+from botocore.session import Session as BotoCoreSession
 
-# set up application metrics
-boto3_session = boto3.Session()
-boto_session = botocore.session.Session()
-
-swf_region = APP_CONFIG.get('botoflow', 'region')
-swf_domain = APP_CONFIG.get('botoflow', 'domain')
+boto_session = BotoCoreSession()
+boto3_session = Session(
+    aws_access_key_id=creds.aws_akid, # pylint: disable=undefined-variable
+    aws_secret_access_key=creds.aws_sak, # pylint: disable=undefined-variable
+    botocore_session=boto_session)
