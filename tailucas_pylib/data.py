e6c65609e97bac5992fc1660de454f9676ed0d80
Tai Lucas <tglucas@gmail.com>
2023-12-29 17:00:47 +0200

    Merge dicts as payload roots.

--- tailucas_pylib/data.py
+++ tailucas_pylib/data.py
@@ -11,7 +11,10 @@ log = logging.getLogger(APP_NAME)  # type: ignore
 def make_payload(timestamp=None, data=None, pack=True):
     payload = {'timestamp': make_timestamp(timestamp=timestamp, make_string=True)}
     if data is not None and len(data) > 0:
-        payload['data'] = data
+        if isinstance(data, dict):
+            payload.update(data)
+        else:
+            payload['data'] = data
     if log.level == logging.DEBUG:
         try:
             log.debug(json.dumps(payload))
